a0ecd9a1fe38f52affea222c1569dffd4d376beb
Chriggan <christian.t11111@gmail.com>
2024-05-27 11:48:52 +0200

    App displays components

--- training-planner-web/src/App.tsx
+++ training-planner-web/src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./styles/App.css";
+import { Drill } from "./types/drill";
+import DrillPreviewCard from "./components/DrillPreviewCard";
 
 function App() {
+  const sampleDrill = {
+    id: "3qhe897e",
+    name: "co-op rally",
+    description: `
+      keep the rally going with the intended techniques used during a normal match. 
+      Ideally the first touch would be a bump, 
+      second would be a set and 
+      third a soft attack or tip
+      `,
+    atleastMinutes: 5,
+    recommendedMinutes: 10,
+    skills: ["Passing", "Reception", "Setting", "Tipping", "Attacking"],
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Components of vbsessionplanner</h1>
+      <section>
+        <h2>Drill Component</h2>
+        <DrillPreviewCard drill={sampleDrill} />
+      </section>
+    </main>
   );
 }
 
