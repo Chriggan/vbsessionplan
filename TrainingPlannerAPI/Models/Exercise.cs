915a6ab0b7c08742c8e97055a9b45d7911b9bbc8
Chriggan <christian.t11111@gmail.com>
2024-05-27 10:21:46 +0200

    model update

--- TrainingPlannerAPI/Models/Exercise.cs
+++ TrainingPlannerAPI/Models/Exercise.cs
@@ -6,17 +6,21 @@ public class Exercise
     public int Id { get; set; }
     public string Title { get; set; } = "";
     public string Instructions { get; set; } = "";
-    public Skills PrimarySkill { get; set; } = Skills.Passing;
-    public Skills[] SecondarySkills { get; set; } = [];
-    public Blob GIFImage { get; set; } = new Blob();
+    public int ParticipantsMin { get; set; }
+    public int? ParticipantsMax { get; set; }
+    public int? MinimumDuration { get; set; }
+    public int? RecommendedDuration { get; set; }
+    // public Skills PrimarySkill { get; set; } = Skills.Passing;
+    // public Skills[] SecondarySkills { get; set; } = [];
+    public Blob GIFImage { get; set; }
 }
 
 
-public enum Skills
-{
-    Passing,
-    Setting,
-    Attacking,
-    Serving,
-    Blocking
-}
+// public enum Skills
+// {
+//     Passing,
+//     Setting,
+//     Attacking,
+//     Serving,
+//     Blocking
+// }
